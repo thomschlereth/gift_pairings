2631b8f64075aacbc897a1b54cce86f041ca59f8
Ali Schlereth <aschlere@sbcglobal.net>
2017-11-06 11:50:31 -0700

    Add first login test:

--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -6,7 +6,7 @@ describe 'User visits homepage' do
     user = User.create(first_name: "First",
                         last_name: "Last",
                         username: "Username",
-                        password: "password")
+                        password: "notpassword")
     event.occasions_users.create(user: user)
 
     visit '/'
@@ -14,11 +14,36 @@ describe 'User visits homepage' do
     click_link 'Login'
 
     fill_in 'username', with: user.username
-    fill_in 'password', with: 'password'
+    fill_in 'password', with: 'notpassword'
 
     click_button 'Login'
 
     expect(current_path).to eq(user_path(user))
 
+  end
+
+  it "User with password of password is redirect to change password" do
+    event = Occasion.create(title: "Test")
+    user = User.create(first_name: "First",
+                        last_name: "Last",
+                        username: "Username",
+                        password: "password")
+    event.occasions_users.create(user: user)
+
+    visit '/'
+
+    click_link 'Login'
+    fill_in 'username', with: user.username
+    fill_in 'password', with: 'password'
+
+    click_button 'Login'
+
+    expect(current_path).to eq(edit_user_path(user))
+    expect(page).to have_content("Since this is your first visit, please update your password before continuing.")
+
+    fill_in 'password', with: "newpassword"
+    click_button "Update User"
+
+    expect(current_path).to eq user_path(user)
   end
 end
