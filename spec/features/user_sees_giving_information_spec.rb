0ada4d04143bd8448e02140a4a56edc20bd60eac
Thom <thomschlereth@gmail.com>
2017-11-05 17:27:27 -0700

    Add user show spec

---
+++ spec/features/user_sees_giving_information_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+
+describe "user visits show" do
+  it "they see who they give to" do
+    reciever = User.create(first_name: "Reciever",
+                            last_name: "Last",
+                            username: "Receiver",
+                            password: "password")
+
+    user = User.create(fist_name: "First",
+                        last_name: "Last",
+                        username: "Username",
+                        reciever: reciever,
+                        password: "password")
+
+    visit user_path(user)
+
+    expect(page).to have_content("Welcome #{user.first_name} #{user.last_name}")
+    expect(page).to have_content("You will be giving to #{reciever.first_name} #{reciever.last_name}")
+  end
+end
