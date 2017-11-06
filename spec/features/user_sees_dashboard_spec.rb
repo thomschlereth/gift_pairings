e7b5c7e5a49003f5bc18dca0009ecf2fdda2b11c
Ali Schlereth <aschlere@sbcglobal.net>
2017-11-06 11:37:56 -0700

    Update user dashboard for db updates

--- spec/features/user_sees_dashboard_spec.rb
+++ spec/features/user_sees_dashboard_spec.rb
@@ -2,16 +2,17 @@ require "rails_helper"
 
 describe "user visits show" do
   it "they see who they give to" do
+    event = Occasion.create(title: "test")
     reciever = User.create(first_name: "Reciever",
                             last_name: "Last",
                             username: "Receiver",
                             password: "password")
-
     user = User.create(first_name: "First",
                         last_name: "Last",
                         username: "Username",
-                        reciever: reciever,
                         password: "password")
+    occasion_user_reciever = event.occasions_users.create(user: reciever)
+    event.occasions_users.create(user: user, reciever: occasion_user_reciever)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
