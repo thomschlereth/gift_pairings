25d838deba8fd2313b090634c7eb02888a559842
Thom <thomschlereth@gmail.com>
2017-11-05 21:08:27 -0700

    Add login test

---
+++ spec/features/user_can_login_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'User visits homepage' do
+  it 'User clicks login' do
+    user = User.create(first_name: "First",
+                        last_name: "Last",
+                        username: "Username",
+                        password: "password")
+
+    visit '/'
+
+    click_link 'login'
+
+    fill_in 'username', with: user.username
+    fill_in 'password', with: 'password'
+
+    click 'Login'
+
+    expect(current_path).to eq(user_path(user))
+
+  end
+end
